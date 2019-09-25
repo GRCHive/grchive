a395915768aef7127dcaac104a85280ba97b0243
Michael Bao <halo343gs@gmail.com>
2019-09-25 14:48:19 -0400

    Serve basic index page

--- src/webserver/main.go
+++ src/webserver/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
+
+	dir, _ := os.Getwd()
+	fmt.Println(dir)
+
+	r.LoadHTMLGlob("src/webserver/templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
 
 	// TODO: Configurable port?
 	r.Run(":8080")
