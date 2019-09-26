e339c1b8422cf8da9a219fa6e57af45f9e69d373
Michael Bao <halo343gs@gmail.com>
2019-09-26 19:31:15 -0400

    flesh out getting started form

---
+++ src/core/jsui/vue/components/CollectEmail.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-form>
+        <v-text-field
+            label="Name"
+            filled
+        ></v-text-field>
+
+        <v-text-field
+            label="Email"
+            filled
+        ></v-text-field>
+
+        <v-switch
+            :label="`I agree to ${companyName} collecting and storing my personal information to send me updates about future services and products.`"
+        >
+        </v-switch>
+
+        <v-btn
+            color="success"
+        >
+            Submit
+        </v-btn>
+
+    </v-form>
+</template>
+
+<script lang="ts">
+
+export default {
+    props: [
+        'companyName'
+    ]
+}
+
+</script>
--- src/core/jsui/vue/pages/GettingStartedPage.vue
+++ src/core/jsui/vue/pages/GettingStartedPage.vue
@@ -6,7 +6,41 @@
         </landing-page-app-bar>
 
         <v-content>
-            <h1>Get Started</h1>
+            <v-container fluid class="pa-0 max-height">
+                <v-row 
+                    align="center"
+                    class="max-height"
+                >
+                    <v-col cols=8 class="pa-3">
+                        <v-row justify="center">
+                            <v-card
+                                width="80%"
+                            >
+                                <v-card-title>
+                                    Thank you for your interest!
+                                </v-card-title>
+                                <v-card-text id="message">
+                                    Our engineers are hard at work getting {{ companyName }} ready for you.
+                                    Leave us your name and email address, and we will contact you with more information on how we can help you simplify your external and internal auditing processes.
+                                </v-card-text>
+                                <v-col cols=12>
+                                    <collect-email></collect-email>
+                                </v-col>
+                            </v-card>
+                        </v-row>
+                    </v-col>
+
+                    <v-col cols=4 class="pa-0 max-height">
+                        <v-img
+                            :src="imageUrl"
+                            min-height="100%"
+                            max-height="100%"
+                        >
+                        </v-img>
+                    </v-col>
+                </v-row>
+
+            </v-container>
         </v-content>
     </v-app>
 </template>
@@ -14,6 +48,8 @@
 <script lang="ts">
 
 import LandingPageAppBar from '../components/LandingPageAppBar.vue'
+import CollectEmail from '../components/CollectEmail.vue'
+import { createAssetUrl} from '../../ts/url'
 
 export default {
     props: [
@@ -21,7 +57,19 @@ export default {
     ],
     components: {
         LandingPageAppBar,
+        CollectEmail
+    },
+    computed: {
+        imageUrl: () => createAssetUrl("construct.jpg")
     }
 }
 
 </script>
+
+<style scoped>
+
+#message {
+    word-break: keep-all;
+}
+
+</style>
