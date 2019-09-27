564637e2763f9d6605685889f14fb5507daa9948
Michael Bao <halo343gs@gmail.com>
2019-09-26 21:03:50 -0400

    relayout getting started to fix bug in firefox

--- src/core/jsui/vue/pages/GettingStartedPage.vue
+++ src/core/jsui/vue/pages/GettingStartedPage.vue
@@ -9,37 +9,36 @@
             <v-container fluid class="pa-0 max-height">
                 <v-row 
                     align="center"
+                    justify="center"
                     class="max-height"
                 >
-                    <v-col cols=8 class="pa-3">
-                        <v-row justify="center">
-                            <v-card
-                                width="80%"
-                            >
-                                <v-card-title>
-                                    Thank you for your interest!
-                                </v-card-title>
-                                <v-card-text id="message">
-                                    Our engineers are hard at work getting {{ companyName }} ready for you.
-                                    Leave us your name and email address, and we will contact you with more information on how we can help you simplify your external and internal auditing processes.
-                                </v-card-text>
-                                <v-col cols=12>
-                                    <collect-email></collect-email>
+                        <v-card width="80%">
+                            <v-row>
+                                <v-col cols=8>
+                                    <v-card-title>
+                                        Thank you for your interest!
+                                    </v-card-title>
+                                    <v-card-text id="message">
+                                        Our engineers are hard at work getting {{ companyName }} ready for you.
+                                        Leave us your name and email address, and we will contact you with more information on how we can help you simplify your external and internal auditing processes.
+                                    </v-card-text>
+                                    <v-col cols=12>
+                                        <collect-email :company-name="companyName"></collect-email>
+                                    </v-col>
                                 </v-col>
-                            </v-card>
-                        </v-row>
-                    </v-col>
-
-                    <v-col cols=4 class="pa-0 max-height">
-                        <v-img
-                            :src="imageUrl"
-                            min-height="100%"
-                            max-height="100%"
-                        >
-                        </v-img>
-                    </v-col>
+                                <v-col
+                                    cols=4
+                                    class="pa-0 hide-overflow"
+                                >
+                                    <v-img 
+                                        :src="imageUrl"
+                                        height="100%"
+                                        class="pa-0"
+                                    ></v-img>
+                                </v-col>
+                            </v-row>
+                        </v-card>
                 </v-row>
-
             </v-container>
         </v-content>
     </v-app>
