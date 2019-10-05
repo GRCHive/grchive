0b57b799ac36b2801cc4d12a20bff4bd5584bbc8
Michael Bao <halo343gs@gmail.com>
2019-10-05 00:36:53 -0400

    start profile editor

---
+++ src/core/jsui/vue/components/dashboard/UserProfileViewEditor.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-form>
+        <v-text-field v-model="firstName" label="First Name">
+        </v-text-field>
+        <v-text-field v-model="lastName" label="Last Name">
+        </v-text-field>
+        <v-text-field v-model="email" label="Email">
+        </v-text-field>
+    </v-form>
+
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend({
+    data: function() {
+        return {
+            //@ts-ignore
+            firstName: this.$root.userFirstName,
+            //@ts-ignore
+            lastName: this.$root.userLastName,
+            //@ts-ignore
+            email: this.$root.userEmail,
+        }
+    }
+})
+
+</script>
--- src/core/jsui/vue/pages/dashboard/DashboardUserHome.vue
+++ src/core/jsui/vue/pages/dashboard/DashboardUserHome.vue
@@ -5,6 +5,7 @@
 
         <v-content class="max-height">
             <user-home-page-nav-bar :selected-page="0"></user-home-page-nav-bar>
+            <user-profile-view-editor></user-profile-view-editor>
         </v-content>
 
     </section>
@@ -14,10 +15,12 @@
 
 import DashboardAppBar from '../../components/dashboard/DashboardAppBar.vue'
 import UserHomePageNavBar from '../../components/dashboard/UserHomePageNavBar.vue'
+import UserProfileViewEditor from '../../components/dashboard/UserProfileViewEditor.vue'
 export default {
     components : {
         DashboardAppBar,
-        UserHomePageNavBar
+        UserHomePageNavBar,
+        UserProfileViewEditor
     }
 }
 </script>
