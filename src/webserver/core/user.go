e51f06f5d97453037169ae8d09380ac67099b435
Michael Bao <halo343gs@gmail.com>
2019-10-05 15:56:45 -0400

    clarifying comment

--- src/webserver/core/user.go
+++ src/webserver/core/user.go
@@ -1,6 +1,10 @@
 package core
 
 type User struct {
+	// We don't store the full name of the user from Okta because
+	// apparently after you do a user update and request another ID token,
+	// the first/last name will get updated but not the full name. So
+	// piece together their full name ourselves.
 	FirstName   string
 	LastName    string
 	Email       string
