5954bd605414cb8629b760c70954ced92fce526e
Michael Bao <halo343gs@gmail.com>
2019-10-30 10:41:31 -0400

    email validate test (+better email regex)

---
+++ test/webserver/core/validate_test.go
@@ -0,0 +1,48 @@
+package core_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/b3h47pte/audit-stuff/core"
+	"testing"
+)
+
+func TestValidateEmailFormat(t *testing.T) {
+	// Test cases source:
+	// https://blogs.msdn.microsoft.com/testing123/2009/02/06/email-address-test-cases/
+	for _, ref := range []struct {
+		email string
+		valid bool
+	}{
+		{`email@domain.com`, true},
+		{`firstname.lastname@domain.com`, true},
+		{`email@subdomain.domain.com`, true},
+		{`firstname+lastname@domain.com`, true},
+		{`email@123.123.123.123`, true},
+		{`email@[123.123.123.123]`, true},
+		{`"email"@domain.com`, true},
+		{`1234567890@domain.com`, true},
+		{`email@domain-one.com`, true},
+		{`_______@domain.com`, true},
+		{`email@domain.name`, true},
+		{`email@domain.co.jp`, true},
+		{`firstname-lastname@domain.com`, true},
+		{`plainaddress`, false},
+		{`#@%^%#$@#$@#.com`, false},
+		{`@domain.com`, false},
+		{`Joe Smith <email@domain.com>`, false},
+		{`email.domain.com`, false},
+		{`email@domain@domain.com`, false},
+		{`.email@domain.com`, false},
+		{`email.@domain.com`, false},
+		{`email..email@domain.com`, false},
+		{`あいうえお@domain.com`, false},
+		{`email@domain.com (Joe Smith)`, false},
+		{`email@domain`, false},
+		{`email@-domain.com`, false},
+		{`email@domain..com`, false},
+	} {
+		valid := core.ValidateEmailFormat(ref.email)
+		assert.Equal(t, ref.valid, valid, ref.email)
+	}
+
+}
