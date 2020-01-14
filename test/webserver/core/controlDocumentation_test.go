e1e60a8bb7021a8561d928a137ccc4b4064cc288
Michael Bao <halo343gs@gmail.com>
2020-01-14 18:15:19 -0500

    control documentation test

---
+++ test/webserver/core/controlDocumentation_test.go
@@ -0,0 +1,61 @@
+package core_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/b3h47pte/audit-stuff/core"
+	"testing"
+)
+
+func TestUniqueKey(t *testing.T) {
+	for _, ref := range []struct {
+		file core.ControlDocumentationFile
+		key  string
+	}{
+		{
+			file: core.ControlDocumentationFile{
+				Id: 3,
+			},
+			key: "controlDocFile-3",
+		},
+		{
+			file: core.ControlDocumentationFile{
+				Id: 392,
+			},
+			key: "controlDocFile-392",
+		},
+	} {
+		assert.Equal(t, ref.key, ref.file.UniqueKey())
+	}
+}
+
+func TestStorageFilename(t *testing.T) {
+	for _, ref := range []struct {
+		file     core.ControlDocumentationFile
+		org      core.Organization
+		filename string
+	}{
+		{
+			file: core.ControlDocumentationFile{
+				Id: 3,
+			},
+			org: core.Organization{
+				Id:   1,
+				Name: "Test",
+			},
+			filename: "org-1-Test/controlDocFile-3",
+		},
+		{
+			file: core.ControlDocumentationFile{
+				Id: 392,
+			},
+			org: core.Organization{
+				Id:   3,
+				Name: "Blah Blah",
+			},
+			filename: "org-3-Blah Blah/controlDocFile-392",
+		},
+	} {
+		assert.Equal(t, ref.filename, ref.file.StorageFilename(&ref.org))
+	}
+
+}
