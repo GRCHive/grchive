ca694c45bc6a30a2f8e3a07d29bfbb4f3062607d
Michael Bao <halo343gs@gmail.com>
2020-01-15 12:01:01 -0500

    search test

---
+++ test/webserver/core/search_test.go
@@ -0,0 +1,38 @@
+package core_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/b3h47pte/audit-stuff/core"
+	"testing"
+)
+
+func TestLinearSearchInt32Slice(t *testing.T) {
+	for _, ref := range []struct {
+		slice  []int32
+		search int32
+		index  int
+	}{
+		{
+			slice:  []int32{5, 3, 2, 10},
+			search: 2,
+			index:  2,
+		},
+		{
+			slice:  []int32{5, 3, 2, 10},
+			search: 5,
+			index:  0,
+		},
+		{
+			slice:  []int32{5, 3, 2, 10},
+			search: 10,
+			index:  3,
+		},
+		{
+			slice:  []int32{5, 3, 2, 10},
+			search: 22,
+			index:  core.SearchNotFound,
+		},
+	} {
+		assert.Equal(t, ref.index, core.LinearSearchInt32Slice(ref.slice, ref.search))
+	}
+}
