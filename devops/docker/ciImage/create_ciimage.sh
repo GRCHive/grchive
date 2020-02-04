e07500f28be41cb0b68d06bbf7b8f3cd1c7c7314
Michael Bao <mike@grchive.com>
2020-02-04 18:55:43 -0500

    smaller preview generator base (hopefully)

---
+++ devops/docker/ciImage/create_ciimage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE_NAME=$1
+DEPENDENCIES="bootstrap_bazel.sh download_libffi.sh download_python.sh download_terraform.sh python-helper"
+
+# Copy needed files
+for DEPEND in ${DEPENDENCIES};
+do
+    cp -r ../../../dependencies/${DEPEND} .
+done
+
+docker build --tag ${IMAGE_NAME} .
+docker push ${IMAGE_NAME}
+
+# Cleanup
+for DEPEND in ${DEPENDENCIES};
+do
+    rm -rf ${DEPEND}
+done
