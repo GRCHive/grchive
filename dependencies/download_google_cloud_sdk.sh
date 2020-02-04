324180e1b5010412506070400cce557123c56318
Michael Bao <mike@grchive.com>
2020-02-04 18:55:02 -0500

    gcloud sdk dependency

---
+++ dependencies/download_google_cloud_sdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MAJOR_VERSION=279
+MINOR_VERSION=0
+PATCH_VERSION=0
+FULL_VERSION=${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}
+URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${FULL_VERSION}-linux-x86_64.tar.gz"
+
+curl -o gcloud.tar.gz $URL
+
+mkdir -p gcloud
+tar xvf gcloud.tar.gz -C gcloud 
+cd gcloud
+./google-cloud-sdk/install.sh
+cd ../
+rm gcloud.tar.gz
