bf44eadd5a4c548d7d00c44ff3c154c6b9ba0e6c
Michael Bao <mike@grchive.com>
2020-02-04 17:40:47 -0500

    webserver node pool

--- devops/terraform/prod/modules/webserver/main.tf
+++ devops/terraform/prod/modules/webserver/main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "webserver-gke" {
     name     = "webserver-gke-cluster"
-    location = "us-central1"
+    location = "us-central1-c"
 
     remove_default_node_pool = true
     initial_node_count       = 1
@@ -17,11 +17,17 @@ resource "google_container_cluster" "webserver-gke" {
 
 resource "google_container_node_pool" "webserver-node-pool" {
     name        = "webserver-node-pool"
-    location    = "us-central1"
+    location    = "us-central1-c"
     cluster     = google_container_cluster.webserver-gke.name
     node_count  = 1
 
     node_config {
         disk_size_gb        = 10
+        disk_type           = "pd-standard"
+        image_type          = "COS"
+        labels = {
+            app = "webserver"
+        }
+        machine_type = "f1-micro"
     }
 }
