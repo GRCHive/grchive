4e61a8629beb8f6812fd4856b1a135281911093b
Michael Bao <mike@grchive.com>
2020-02-10 17:10:17 -0500

    pull all env variables properly

--- scripts/deploy/pull_env_variables_helper.py
+++ scripts/deploy/pull_env_variables_helper.py
@@ -1,15 +1,25 @@
 import requests
 import os
 
-url = "https://gitlab.com/api/v4/projects/grchive%2Fgrchive/variables"
-token = os.environ["GITLAB_API_TOKEN"]
+def pullData(url):
+    token = os.environ["GITLAB_API_TOKEN"]
 
-r = requests.get(url=url, headers={
-    "PRIVATE-TOKEN" : token  
-})
-data = r.json()
+    r = requests.get(url=url, headers={
+        "PRIVATE-TOKEN" : token  
+    })
+    data = r.json()
+    return data, r.links["next"]["url"] if "next" in r.links else None
 
-for datum in data:
-    key = datum['key']
-    val = datum['value'].replace("\"", "\\\"")
-    print("export {}=\"{}\"".format(key, val))
+initialUrl = "https://gitlab.com/api/v4/projects/grchive%2Fgrchive/variables"
+data, nextUrl = pullData(initialUrl)
+
+while True:
+    for datum in data:
+        key = datum['key']
+        val = datum['value'].replace("\"", "\\\"")
+        print("export {}=\"{}\"".format(key, val))
+
+    if nextUrl == None:
+        break
+
+    data, nextUrl = pullData(nextUrl)
