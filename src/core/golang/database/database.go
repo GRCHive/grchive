c48804968455b872fcfcd9c2d10f8b7edb5cd28a
Michael Bao <mike@grchive.com>
2020-02-11 16:42:10 -0500

    limit number of connections to the db to use CU #4jf8w8[in progress]

--- src/core/golang/database/database.go
+++ src/core/golang/database/database.go
@@ -4,12 +4,16 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"gitlab.com/grchive/grchive/core"
+	"time"
 )
 
 var dbConn *sqlx.DB
 
 func Init() {
 	dbConn = sqlx.MustConnect("postgres", core.EnvConfig.DatabaseConnString)
+	dbConn.SetMaxOpenConns(10)
+	dbConn.SetMaxIdleConns(5)
+	dbConn.SetConnMaxLifetime(5 * time.Minute)
 }
 
 func CreateTx() *sqlx.Tx {
