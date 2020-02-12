b07b15c113651b58e9e7deae5542084ac5b37777
Michael Bao <mike@grchive.com>
2020-02-12 18:30:56 -0500

    forgot to actually clear interval

--- src/core/jsui/vue/generic/DatabaseSqlEditor.vue
+++ src/core/jsui/vue/generic/DatabaseSqlEditor.vue
@@ -176,6 +176,7 @@ export default class DatabaseSqlEditor extends Props {
                     this.selectedRefresh = resp.data
                 }
 
+                clearInterval(intervalId)
             }).catch((err : any) => {})
         }, refreshIntervalSeconds * 1000)
     }
