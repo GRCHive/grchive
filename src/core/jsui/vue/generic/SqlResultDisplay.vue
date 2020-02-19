47cf3974ba02bbf3786d25af702de49bf19716ee
Michael Bao <mike@grchive.com>
2020-02-18 21:48:12 -0500

    support SQL CSV export CU #4pbp1f[closed]

--- src/core/jsui/vue/generic/SqlResultDisplay.vue
+++ src/core/jsui/vue/generic/SqlResultDisplay.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
         <div v-if="result.data.Success">
-            <v-text-field outlined
-                          v-model="filterString"
-                          prepend-inner-icon="mdi-magnify"
-                          label="Search"
-                          hide-details
-                          clearable
-            ></v-text-field>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-text-field outlined
+                                  v-model="filterString"
+                                  prepend-inner-icon="mdi-magnify"
+                                  label="Search"
+                                  hide-details
+                                  clearable
+                    ></v-text-field>
+                </v-list-item-content>
+
+                <v-list-item-action>
+                    <v-btn
+                        icon
+                        @click="exportToCsv"
+                    >
+                        <v-icon>
+                            mdi-export-variant
+                        </v-icon>
+                    </v-btn>
+                </v-list-item-action>
+            </v-list-item>
 
             <v-data-table
                 :headers="headers"
@@ -34,6 +49,7 @@ import Component from 'vue-class-component'
 import { TRunSqlQueryOutput } from '../../ts/api/apiSqlQueries'
 import { SqlResult } from '../../ts/sql'
 import Papa from 'papaparse'
+import { saveAs } from 'file-saver'
 
 const Props = Vue.extend({
     props: {
@@ -47,6 +63,14 @@ const Props = Vue.extend({
 @Component
 export default class SqlResultDisplay extends Props {
     filterString : string = ""
+
+    get parsedData() : any | null {
+        if (!this.result.data.Success) {
+            return null
+        }
+
+        return Papa.parse((<SqlResult>this.result.data.Data).CsvText)
+    }
 
     get headers() : any[] {
         if (!this.result.data.Success) {
@@ -64,8 +88,7 @@ export default class SqlResultDisplay extends Props {
         }
 
         let allColumns = (<SqlResult>this.result.data.Data).Columns
-        let parsedData : any = Papa.parse((<SqlResult>this.result.data.Data).CsvText)
-        return parsedData.data.filter((ele : Array<any>) => ele.length == allColumns.length).map(
+        return this.parsedData.data.filter((ele : Array<any>) => ele.length == allColumns.length).map(
             (ele : Array<any>) => {
                 let obj = Object()
                 for (let i = 0; i < ele.length; ++i) {
@@ -73,6 +96,20 @@ export default class SqlResultDisplay extends Props {
                 }
                 return obj
             })
+    }
+
+    exportToCsv() {
+        if (!this.result.data.Success) {
+            return
+        }
+
+        let columnCsv : string = (<SqlResult>this.result.data.Data).Columns.join(this.parsedData.meta.delimiter)
+        let resultCsv : string = (<SqlResult>this.result.data.Data).CsvText
+        let fullText : string = columnCsv + this.parsedData.meta.linebreak + resultCsv
+        let blob : Blob = new Blob([fullText], {
+            type: 'text/csv;charset=utf-8',
+        })
+        saveAs(blob, 'result.csv')
     }
 }
 
