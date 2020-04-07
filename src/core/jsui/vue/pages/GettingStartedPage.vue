2c7bd29990098e856a0c47e90d0215a26eb4b81d
Michael Bao <mike@grchive.com>
2020-04-06 20:19:25 -0400

    Landing page and learn more pages

--- src/core/jsui/vue/pages/GettingStartedPage.vue
+++ src/core/jsui/vue/pages/GettingStartedPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <div>
         <landing-page-app-bar
             :company-name="companyName"
         >
@@ -14,45 +14,43 @@
             ></hero-image>
 
             <v-container fluid class="pa-0" id="content">
-                <v-row 
-                    align="center"
-                    justify="center"
-                    class="max-height"
-                >
-                    <v-card width="80%" id="card-content" elevation=12 raised>
-                        <v-row
-                            align="center"
-                        >
-                            <v-col cols=8>
-                                <v-card-title>
-                                    Thank you for your interest!
-                                </v-card-title>
-                                <v-card-text class="long-text">
-                                    Our engineers are hard at work getting {{ companyName }} ready for you.
-                                    Leave us your name and email address, and we will contact you with more information on how we can help you streamline your external and internal auditing processes.
-                                </v-card-text>
-                                <v-col cols=12>
-                                    <collect-email 
-                                        :company-name="companyName"
-                                    ></collect-email>
-                                </v-col>
-                            </v-col>
-                            <v-col
-                                cols=4
-                                class="pa-0 hide-overflow"
+                <div class="mainContent">
+                    <v-row 
+                        align="center"
+                        justify="center"
+                        class="max-height"
+                    >
+                        <v-card width="80%" id="card-content" elevation=12 raised>
+                            <v-row
+                                align="center"
                             >
-                                <v-img 
-                                    :src="imageUrl"
-                                    height="100%"
-                                    class="pa-0"
-                                ></v-img>
-                            </v-col>
-                        </v-row>
-                    </v-card>
-                </v-row>
+                                <v-col cols=8>
+                                    <v-card-title>
+                                        Request a live demo.
+                                    </v-card-title>
+                                    <v-col cols=12>
+                                        <collect-email 
+                                            :company-name="companyName"
+                                        ></collect-email>
+                                    </v-col>
+                                </v-col>
+                                <v-col
+                                    cols=4
+                                    class="pa-0 hide-overflow"
+                                >
+                                    <v-img 
+                                        :src="imageUrl"
+                                        height="100%"
+                                        class="pa-0"
+                                    ></v-img>
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-row>
+                </div>
             </v-container>
         </v-content>
-    </section>
+    </div>
 </template>
 
 <script lang="ts">
