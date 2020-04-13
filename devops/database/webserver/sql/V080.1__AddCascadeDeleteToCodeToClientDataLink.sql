91a0189b1a717182316d584cb4adf8f23e906095
Michael Bao <mike@grchive.com>
2020-04-13 15:31:31 -0400

    make migration work on google cloud database

--- devops/database/webserver/sql/V080.1__AddCascadeDeleteToCodeToClientDataLink.sql
+++ devops/database/webserver/sql/V080.1__AddCascadeDeleteToCodeToClientDataLink.sql
@@ -1,12 +1,14 @@
 ALTER TABLE code_to_client_data_link 
-DROP CONSTRAINT code_to_client_data_link_code_id_org_id_fkey,
+DROP CONSTRAINT IF EXISTS code_to_client_data_link_code_id_org_id_fkey,
+DROP CONSTRAINT IF EXISTS code_to_client_data_link_code_id_fkey,
 ADD CONSTRAINT code_to_client_data_link_code_id_org_id_fkey
     FOREIGN KEY(code_id, org_id)
     REFERENCES managed_code(id, org_id)
     ON DELETE CASCADE;
 
 ALTER TABLE code_to_client_data_link 
-DROP CONSTRAINT code_to_client_data_link_data_id_org_id_fkey,
+DROP CONSTRAINT IF EXISTS code_to_client_data_link_data_id_org_id_fkey,
+DROP CONSTRAINT IF EXISTS code_to_client_data_link_data_id_fkey,
 ADD CONSTRAINT code_to_client_data_link_data_id_org_id_fkey
     FOREIGN KEY(data_id, org_id)
     REFERENCES client_data(id, org_id)
