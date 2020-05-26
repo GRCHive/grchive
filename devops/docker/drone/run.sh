3e0891ed5460ee1c2837ca120e0b51230b65ada4
Michael Bao <mike@grchive.com>
2020-05-26 18:28:09 -0400

    tweak...

--- devops/docker/drone/run.sh
+++ devops/docker/drone/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Wait for access to vault to come up..
+sleep 30;
+
 # Pull secrets from Vault.
-VAULT_RESP=$(/app/get_vault_secret_http.sh ${DRONE_GITEA_CLIENT_SECRET_VAULT} || exit 1)
+VAULT_RESP=$(/app/get_vault_secret_http.sh ${DRONE_GITEA_CLIENT_SECRET_VAULT})
 
 
 export DRONE_GITEA_CLIENT_ID=$(echo $VAULT_RESP | jq -j '.id')
